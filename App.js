19f6e8b498196812afd8cd71ea53c3138cf5d2d4
britan712 <64856626+britan712@users.noreply.github.com>
2023-03-22 22:36:28 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,69 @@
+import { SafeAreaView, Text, StyleSheet, Button, Image } from "react-native";
+import React, { useState, useEffect } from "react";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { StatusBar } from "expo-status-bar";
+
+export default function bcScannerApp() {
+  const [permission, setPermision] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  // Perizinan kamera
+  useEffect(() => {
+    const getPermission = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setPermision(status === "granted");
+    };
+    getPermission();
+  }, []);
+
+  // Ambil nilai hasil scan
+  const scannerHandler = ({ data }) => {
+    setScanned(true);
+    alert(`Result: \n ${data}`);
+  };
+
+  if (permission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+
+  if (permission === false) {
+    return <Text>Access Denied!</Text>;
+  }
+  // Styles
+  const styles = StyleSheet.create({
+    body: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "#000",
+    },
+    scanner: {
+      width: "100%",
+      height: "100%",
+      position: "absolute",
+    },
+    image: {
+      position: "absolute",
+    },
+    button: {
+      marginTop: "auto",
+      backgroundColor: "#fff",
+      color: "#000",
+    },
+  });
+
+  // Scan handler
+  function buttonHandler() {
+    return setScanned(false);
+  }
+
+  return (
+    // Tampilkan
+    <SafeAreaView style={styles.body}>
+      <StatusBar style="light" />
+      <BarCodeScanner onBarCodeScanned={scanned ? undefined : scannerHandler} style={styles.scanner} />
+      <Image source={require("./assets/Scan.png")} style={styles.image} />
+      {scanned && <Button title="SCAN AGAIN" onPress={buttonHandler} style={styles.button} />}
+    </SafeAreaView>
+  );
+}
